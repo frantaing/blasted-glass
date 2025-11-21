c59355868c8313f0aa4159d9019c221594520bc8
frantan <francinetaing13@gmail.com>
2025-11-21 21:49:27 +0700

    feat: replace static project with SvelteKit version

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<!-- src/routes/+layout.svelte -->
+<!-- The layout for indexes -->
+
+<script lang="ts">
+	import '../app.css';
+
+	let { children } = $props();
+</script>
+
+<div class="relative w-screen min-h-screen">
+  <!-- Background image -->
+  <img src="/backgrounds/background.gif" alt="non-stretching background" class="absolute z-0 inset-0 w-full h-full object-cover bg-center bg-no-repeat bg-black"/>
+  
+  <!-- Background overlay -->
+  <div class="animate-overlay-on-load z-10 flex justify-center items-center w-full absolute inset-0 p-10 font-general text-white bg-black/90">
+    {@render children()}
+  </div>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<!-- src/routes/+page.svelte -->
+<!-- Homepage index -->
+
+<script lang="ts">
+	import { typewriter } from '$lib/actions/typewriter'; // Apply Typewriter animation
+</script>
+
+<div class="flex flex-col gap-7 w-screen max-w-sm h-screen max-h-80 text-left">
+	<!-- Heading & Description -->
+    <section class="flex flex-col gap-2">
+        <h1 use:typewriter={'hi, welcome to my sandbox!'}>.</h1>
+		<p use:typewriter={'this is where i stash reject (but still cool) stuff i made. you can find all sort of ui elements from buttons to full on layouts.'}></p>
+    </section>
+    <!-- navigation -->
+    <nav class="flex flex-col gap-2 pl-2 border-l-2">
+        <a href="/" class="group navlink">
+            <img src="/assets/arrow.png" alt="Link hover arrow icon" class="hidden w-6 h-5 group-hover:block"/>
+            <span use:typewriter={'layouts'}></span>
+        </a>
+    </nav>
+    <!-- github -->
+    <a href="https://github.com/frantaing/blasted-glass" class="text-sm hover:underline" use:typewriter={'@frantaing'} aria-label="blasted-glass Github repo"></a>
+</div>
