a525514f52e98a77f3f6629344d3b2ff677c2f51
frantan <francinetaing13@gmail.com>
2025-11-22 01:06:01 +0700

    fix: try remove Typewriter

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,23 +2,24 @@
 <!-- Homepage index -->
 
 <script lang="ts">
-	import { typewriter } from '$lib/actions/typewriter'; // Apply Typewriter animation
     import { base } from '$app/paths';
 </script>
 
 <div class="flex flex-col gap-7 w-screen max-w-sm h-screen max-h-80 text-left">
-	<!-- Heading & Description -->
-    <section class="flex flex-col gap-2">
-        <h1 use:typewriter={'hi, welcome to my sandbox!'}>.</h1>
-		<p use:typewriter={'this is where i stash reject (but still cool) stuff i made. you can find all sort of ui elements from buttons to full on layouts.'}></p>
-    </section>
-    <!-- navigation -->
-    <nav class="flex flex-col gap-2 pl-2 border-l-2">
-        <a href="{base}/layouts" class="group navlink">
-            <img src="{base}/assets/arrow.png" alt="Link hover arrow icon" class="hidden w-6 h-5 group-hover:block"/>
-            <span use:typewriter={'layouts'}></span>
+	<!-- heading -->
+    <h1 class="type-me">hi, welcome to my sandbox!</h1>
+    <!-- bottom text -->
+    <div class="flex flex-col gap-5">
+        <!-- description -->
+        <p class="type-me">this is where i stash reject (but still cool) stuff i made. you can find all sort of ui elements from buttons to full on layouts.</p>
+        <!-- navigation -->
+        <nav class="flex flex-col gap-2">
+        <!-- NOTE: Added empty href attributes for valid HTML -->
+        <a href="{base}/layouts" class="group navlink type-me">
+            <img src="{base}/assets/arrow.png" alt="link hover arrow icon" class="hidden w-6 h-5 group-hover:block"/>link
         </a>
-    </nav>
+        </nav>
+    </div>
     <!-- github -->
-    <a href="https://github.com/frantaing/blasted-glass" class="text-sm hover:underline" use:typewriter={'@frantaing'} aria-label="blasted-glass Github repo"></a>
+    <a href="#" class="text-sm hover:underline type-me">@frantaing</a>
 </div>
