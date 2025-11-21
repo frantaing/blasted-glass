41aa9540c986e94125b61c12eb0c33ea85ca9c7f
frantan <francinetaing13@gmail.com>
2025-11-22 01:32:55 +0700

    style: add '//' before  navlinks for ~style~

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,20 +6,19 @@
 </script>
 
 <div class="flex flex-col gap-7 w-screen max-w-sm h-screen max-h-80 text-left">
-	<!-- heading -->
-    <h1 class="type-me">hi, welcome to my sandbox!</h1>
-    <!-- bottom text -->
-    <div class="flex flex-col gap-5">
-        <!-- description -->
-        <p class="type-me">this is where i stash reject (but still cool) stuff i made. you can find all sort of ui elements from buttons to full on layouts.</p>
-        <!-- navigation -->
+	<!-- Heading & Description -->
+    <section>
+        <h1 class="type-me">hi, welcome to my sandbox!</h1>        
+        <p class="type-me">this is where i stash reject (but still cool) stuff i made. you can find all sort of ui elements from buttons to full on layouts.</p>    
+    </section>
+    <!-- Navigation -->
+    <section class="flex flex-col gap-5">
         <nav class="flex flex-col gap-2">
-        <!-- NOTE: Added empty href attributes for valid HTML -->
         <a href="{base}/layouts" class="group navlink type-me">
-            <img src="{base}/assets/arrow.png" alt="link hover arrow icon" class="hidden w-6 h-5 group-hover:block"/>link
+            <img src="{base}/assets/arrow.png" alt="link hover arrow icon" class="hidden w-6 h-5 group-hover:block"/>layouts
         </a>
         </nav>
-    </div>
+    </section>
     <!-- github -->
-    <a href="#" class="text-sm hover:underline type-me">@frantaing</a>
+    <a href="{base}/" class="mt-10 text-sm hover:underline type-me">@frantaing</a>
 </div>
