680f6ec023648e02a7e61a3ae05fda2c4014a01c
frantan <francinetaing13@gmail.com>
2025-11-21 21:54:17 +0700

    chore: configure sveltekit for github pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,13 +3,14 @@
 
 <script lang="ts">
 	import '../app.css';
-
-	let { children } = $props();
+  import { base } from '$app/paths';
+	
+  let { children } = $props();
 </script>
 
 <div class="relative w-screen min-h-screen">
   <!-- Background image -->
-  <img src="/backgrounds/background.gif" alt="non-stretching background" class="absolute z-0 inset-0 w-full h-full object-cover bg-center bg-no-repeat bg-black"/>
+  <img src="{base}/backgrounds/background.gif" alt="non-stretching background" class="absolute z-0 inset-0 w-full h-full object-cover bg-center bg-no-repeat bg-black"/>
   
   <!-- Background overlay -->
   <div class="animate-overlay-on-load z-10 flex justify-center items-center w-full absolute inset-0 p-10 font-general text-white bg-black/90">
--- src/routes/layouts/+page.svelte
+++ src/routes/layouts/+page.svelte
@@ -1,20 +1,22 @@
 <!-- src/routes/+page.svelte -->
-<!-- Homepage index -->
+<!-- Layout index -->
 
 <script lang="ts">
 	import { typewriter } from '$lib/actions/typewriter'; // Apply Typewriter animation
     import { base } from '$app/paths';
 </script>
 
+<!-- TODO: CHANGE THIS FROM COPIED HOMEPAGE -->
+
 <div class="flex flex-col gap-7 w-screen max-w-sm h-screen max-h-80 text-left">
 	<!-- Heading & Description -->
     <section class="flex flex-col gap-2">
-        <h1 use:typewriter={'hi, welcome to my sandbox!'}>.</h1>
+        <h1 use:typewriter={'hferferer'}>.</h1>
 		<p use:typewriter={'this is where i stash reject (but still cool) stuff i made. you can find all sort of ui elements from buttons to full on layouts.'}></p>
     </section>
     <!-- navigation -->
     <nav class="flex flex-col gap-2 pl-2 border-l-2">
-        <a href="{base}/layouts" class="group navlink">
+        <a href="{base}/" class="group navlink">
             <img src="{base}/assets/arrow.png" alt="Link hover arrow icon" class="hidden w-6 h-5 group-hover:block"/>
             <span use:typewriter={'layouts'}></span>
         </a>
