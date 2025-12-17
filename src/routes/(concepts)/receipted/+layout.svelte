aa657c159edae8fec41b58b29fc269a8434746e5
frantan <francinetaing13@gmail.com>
2025-12-18 00:01:04 +0700

    feat: create receipt image and entry components

--- src/routes/(concepts)/receipted/+layout.svelte
+++ src/routes/(concepts)/receipted/+layout.svelte
@@ -11,54 +11,27 @@
 <NavBar />
 
 <!-- main page -->
-<div class="receipt-theme mt-28 mx-10 text-sm text-justify md:mx-[20%] xl:mx-[34%]">
+<div class="receipt-theme mt-28 mx-[15%] text-sm text-justify md:mx-[20%] xl:mx-[34%]">
     {@render children()}
 </div>
 
-<div class="pb-5 font-bold italic text-xs text-center">
+<div class="py-5 font-bold font-[monospace] italic text-xs text-center">
     <p>Frantaing @2025</p>
 </div>
 
 
 <style>
     /* global styles for this layout only */
-    
     .receipt-theme {
         font-family: monospace;
     }
-
     .receipt-theme :global(h1) {
         text-align: center;
         font-weight: 900;
         padding-bottom: 40px;
         font-size: 2em;
     }
-
-    .receipt-theme :global(h3) {
-        text-align: center;
-        font-style: italic;
-        font-size: 1.17em;
-        margin-top: 20px;
-    }
-
-    /* move this to its own component for img! */
-    .receipt-theme :global(img) {
-        display: block;
-        margin: 0 auto;
-        width: 300px;
-        height: auto;
-        border: 1px solid black;
-        padding: 10px;
-        box-shadow: 6px 6px 0 0 rgb(63, 63, 63, 0.3);
-        transition: box-shadow .2s ease-in;
-    }
-
-    .receipt-theme :global(img:hover) {
-        box-shadow: 10px 10px 0 0 yellow;
-    }
-    
-    /* global scrollbar hide */
-    :global(*::-webkit-scrollbar) {
+    :global(*::-webkit-scrollbar) { /* global scrollbar hide */
         display: none;
     }
 </style>
--- src/routes/(concepts)/receipted/+page.svelte
+++ src/routes/(concepts)/receipted/+page.svelte
@@ -1,89 +1,52 @@
-<!-- title -->
+<script lang="ts">
+    // components
+    import ReceiptEntry from '$lib/components/receipted/ReceiptEntry.svelte';
+</script>
+
+<!-- website title -->
 <svelte:head>
     <title>concept: receipted</title>
 </svelte:head>
 
-<!-- HTML -->
-<div>
+<div class="flex flex-col gap-10">
     <!-- intro (make its own component?) -->
-    <div class="intro">
+    <div class="py-2.5">
         <!-- heading -->
         <h1>Welcome to the Receipt Archive!</h1>
         <!-- description -->
-        <p>
-            This is a passion project where
-            I collect receipts from various
-            purchases I've made. 
-            <br><br>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec diam nisl, vulputate ac tincidunt et, ultricies non massa. Vestibulum lacinia odio purus, a congue lacus rhoncus vitae. Sed vitae ex eget elit lacinia facilisis. Curabitur ac risus non quam cursus condimentum. Mauris vitae elit et nulla maximus dapibus at sed est. Proin tempus enim ut orci porta ullamcorper. Sed sem eros, sollicitudin tincidunt nunc ut, scelerisque sollicitudin nibh.
-        </p>
+        <div class="flex flex-col gap-5">
+            <p>This is a concept for a website where receipts could be scanned, uploaded, and stored. I had a phase where I was <span class="italic">obsessed</span> with collecting receipts. I started making this site, but never got further than a few pages as a concept. I liked it though, and it was one of the very first projects I've made when I first started getting into web development.</p>
+        </div>
     </div>
 
-    <!-- maybe change this to padding instead? -->
-    <br><br>
     <!-- line -->
     <hr />
-    <br><br>
 
     <!-- latest perchases (main content) -->
     <div class="restofcont">
-
         <!-- title -->
         <h1>Latest Purchases</h1>
-        
-        
-        <!-- !!![add a div container for each receipt item]!!! -->
-        <!-- first image -->
-        <div class="imgpad">
-            <!-- Ensure images are in /static/receipts/ -->
-            <img src="receipted/receipts/receipt-1.jpg" alt="Receipt 1">
+        <!-- latest receipts -->
+        <div class="flex flex-col gap-20">
+            <ReceiptEntry 
+                src="receipted/receipts/receipt-1.jpg"
+                title="Receipt 1"
+                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec diam nisl, vulputate ac tincidunt et, ultricies non massa. Vestibulum lacinia odio purus, a congue lacus rhoncus vitae. Sed vitae ex eget elit lacinia facilisis. Curabitur ac risus non quam cursus condimentum. Mauris vitae elit et nulla maximus dapibus at sed est. Proin tempus enim ut orci porta ullamcorper. Sed sem eros, sollicitudin tincidunt nunc ut, scelerisque sollicitudin nibh."
+            />
+            <ReceiptEntry 
+                src="receipted/receipts/receipt-2.jpg"
+                title="Receipt 2"
+                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec diam nisl, vulputate ac tincidunt et, ultricies non massa. Vestibulum lacinia odio purus, a congue lacus rhoncus vitae. Sed vitae ex eget elit lacinia facilisis. Curabitur ac risus non quam cursus condimentum. Mauris vitae elit et nulla maximus dapibus at sed est. Proin tempus enim ut orci porta ullamcorper. Sed sem eros, sollicitudin tincidunt nunc ut, scelerisque sollicitudin nibh."
+            />
+            <ReceiptEntry 
+                src="receipted/receipts/receipt-3.jpg"
+                title="Receipt 3"
+                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec diam nisl, vulputate ac tincidunt et, ultricies non massa. Vestibulum lacinia odio purus, a congue lacus rhoncus vitae. Sed vitae ex eget elit lacinia facilisis. Curabitur ac risus non quam cursus condimentum. Mauris vitae elit et nulla maximus dapibus at sed est. Proin tempus enim ut orci porta ullamcorper. Sed sem eros, sollicitudin tincidunt nunc ut, scelerisque sollicitudin nibh."
+            />            
         </div>
-        
-        <br>
-        
-        <!-- receipt title -->
-        <h3>Receipt 1</h3>
-        
-        <br>
-            
-        <!-- receipt description -->
-        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec diam nisl, vulputate ac tincidunt et, ultricies non massa. Vestibulum lacinia odio purus, a congue lacus rhoncus vitae. Sed vitae ex eget elit lacinia facilisis. Curabitur ac risus non quam cursus condimentum. Mauris vitae elit et nulla maximus dapibus at sed est. Proin tempus enim ut orci porta ullamcorper. Sed sem eros, sollicitudin tincidunt nunc ut, scelerisque sollicitudin nibh.</p>
-        
-        <!-- BREAK! -->
-        <br><br><br>
-
-        <!-- receipt item... -->
-        <div class="imgpad">
-            <img src="receipted/receipts/receipt-2.jpg" alt="Receipt 2">
-        </div>
-        <br>
-        <h3>Receipt 2</h3>
-        <br>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec diam nisl, vulputate ac tincidunt et, ultricies non massa. Vestibulum lacinia odio purus, a congue lacus rhoncus vitae. Sed vitae ex eget elit lacinia facilisis. Curabitur ac risus non quam cursus condimentum. Mauris vitae elit et nulla maximus dapibus at sed est. Proin tempus enim ut orci porta ullamcorper. Sed sem eros, sollicitudin tincidunt nunc ut, scelerisque sollicitudin nibh.</p>
-
-        <!-- BREAK! -->
-        <br><br><br>
-
-        <!-- receipt item... -->
-        <div class="imgpad">
-            <img src="receipted/receipts/receipt-3.jpg" alt="Receipt 3">
-        </div>
-        <br>
-        <h3>Receipt 3</h3>
-        <br>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec diam nisl, vulputate ac tincidunt et, ultricies non massa. Vestibulum lacinia odio purus, a congue lacus rhoncus vitae. Sed vitae ex eget elit lacinia facilisis. Curabitur ac risus non quam cursus condimentum. Mauris vitae elit et nulla maximus dapibus at sed est. Proin tempus enim ut orci porta ullamcorper. Sed sem eros, sollicitudin tincidunt nunc ut, scelerisque sollicitudin nibh.</p>
     </div>
 
-    <!-- BREAK! -->
-    <br><br><br>
     <!-- line -->
     <hr />
-    <br>
+    
 </div>
-
-<!-- gut this for tailwind... -->
-<style>
-    .intro{
-        padding: 10px 0;
-    }
-</style>
