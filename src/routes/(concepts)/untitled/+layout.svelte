bfdee3618b560cb77fa454320162b5ca2ee349eb
frantan <francinetaing13@gmail.com>
2026-01-06 15:02:27 +0700

    chore: setup Untitled concept and link

---
+++ src/routes/(concepts)/untitled/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    let { children } = $props();
+</script>
+
+<div class="untitled-theme">
+    {@render children()}
+</div>
+
+<style>
+    .untitled-theme {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        height: 100vh;
+        background-color: white;
+    }
+</style>
---
+++ src/routes/(concepts)/untitled/+page.svelte
@@ -0,0 +1,8 @@
+<!-- page title -->
+<svelte:head>
+    <title>concept: receipted</title>
+</svelte:head>
+
+<div class="">
+    <h1>hello! this is a test!</h1>
+</div>
