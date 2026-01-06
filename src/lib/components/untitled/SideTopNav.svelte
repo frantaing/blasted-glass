a752111813e093d9e2c109d3a7b6032077c9f535
frantan <francinetaing13@gmail.com>
2026-01-06 22:49:00 +0700

    feat: create navigation sidebar/topbar component for Untitled

---
+++ src/lib/components/untitled/SideTopNav.svelte
@@ -0,0 +1,11 @@
+<nav class="flex sm:flex-col gap-2 sm:w-34 lg:w-3xs">
+    <a href="#" class="text-[#0000EE] underline">link 1</a>
+    <a href="#" class="text-[#0000EE] underline">link 2</a>
+    <a href="#" class="text-[#0000EE] underline">link 3</a>
+    <a href="#" class="text-[#0000EE] underline">link 4</a>
+    <a href="#" class="text-[#0000EE] underline">link 5</a>
+</nav>
+
+<style>
+    
+</style>
--- src/routes/(concepts)/untitled/+layout.svelte
+++ src/routes/(concepts)/untitled/+layout.svelte
@@ -1,17 +1,20 @@
 <script lang="ts">
+    // components
+    import SideTopNav from "$lib/components/untitled/SideTopNav.svelte";
+    
     let { children } = $props();
 </script>
 
-<main class="untitled-theme">
-    <div class="flex flex-col justify-center items-center gap-8 mx-10 w-full border-2 border-gray-500 sm:max-w-3xl">
+<main class="untitled-theme flex justify-center">
+    <div class="flex flex-col gap-8 m-10 w-full sm:max-w-3xl">
         <section class="w-full text-center bg-red-200">
             all-watching eye here...
         </section>
         <section class="flex flex-col justify-between gap-5 w-full sm:flex-row">
-            <div class="sm:w-40 md:w-3xs">
-                <a href="#" class="text-[#0000EE] underline">hello</a>
-            </div>
-            <div class="w-full bg-amber-200">
+            <!-- nav -->
+            <SideTopNav />
+            <!-- main content -->
+            <div class="w-full">
                 {@render children()}       
             </div>
         </section>   
@@ -20,9 +23,6 @@
 
 <style>
     .untitled-theme {
-        display: flex;
-        justify-content: center;
-        align-items: center;
         width: 100%;
         height: 100vh;
         background-color: white;
