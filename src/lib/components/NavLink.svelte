f2991d383688332c8dc9a4b07394953a07c06b58
frantan <francinetaing13@gmail.com>
2026-01-25 14:40:07 +0700

    feat: create separate components for project/index navlinks and for repo links

---
+++ src/lib/components/NavLink.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    // Imports
+    import { typewriter } from '$lib/actions/typewriter';
+
+    // URL, link text
+    let { href, text } = $props();
+</script>
+
+<a {href} class="group navlink flex items-center gap-1 hover:bg-white">
+    <!-- Sepparator -->
+    <span class="type-me mr-1 text-xl font-black group-hover:hidden" use:typewriter={'//'}></span>
+    <!-- The hover icon -->
+    <img src="/assets/arrow.png" alt="link hover arrow icon" class="hidden w-6 h-5 group-hover:block"/>
+    <!-- The text -->
+    <span class="group type-me flex items-center group-hover:text-black" use:typewriter={text}></span>
+</a>
---
+++ src/lib/components/RepoLink.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    // Imports
+    import { typewriter } from '$lib/actions/typewriter';
+    
+    // URL
+    let { href } = $props();
+</script>
+
+<a {href} class="group navlink-sec flex items-center gap-1 hover:bg-white">
+    <!-- The separator -->
+    <span class="type-me mr-1 text-xl font-black group-hover:hidden" use:typewriter={'|'}></span>
+    <!-- The hover icon -->
+    <img src="/assets/arrow.png" alt="link hover arrow icon" class="hidden w-6 h-5 group-hover:block"/>
+    <!-- The text -->
+    <span class="group type-me flex items-center group-hover:text-black" use:typewriter={'</>'}></span>
+</a>
