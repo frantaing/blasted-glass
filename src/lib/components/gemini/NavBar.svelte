90e0a3d2c33e52b2a148d57db8fc7a20e998f0c0
frantan <francinetaing13@gmail.com>
2026-01-29 00:33:55 +0700

    refactor: establish gemini theme; refine NavBar spacing; remove scss

--- src/lib/components/gemini/NavBar.svelte
+++ src/lib/components/gemini/NavBar.svelte
@@ -1,78 +1,83 @@
 <script lang="ts">
-    // Imports
+	// Imports
 </script>
 
-<nav class="flex justify-center items-center w-full h-fit px-4 xs:px-10 md:px-12 pl-6 xs:pl-12 py-2.5 font-medium text-sm text-white bg-black">
-    <div class="flex justify-between items-center w-full xl:max-w-304">
-    <!-- Left: logo + title -->
-    <section class="flex items-center gap-1">
-        <img src="gemini/logo.png" alt="Logo" class="w-6.5 xs:w-5.5"/>
-        <span class="hidden xs:block font-semibold mt-0.5">GEMINI</span>
-    </section>
+<nav
+	class="flex justify-center items-center w-full h-fit px-5 xs:px-[45px] md:px-12 lg:px-[6%] pl-6 xs:pl-12 py-[11px] font-medium text-sm text-primary bg-secondary"
+>
+	<div class="flex justify-between items-center w-full xl:max-w-304">
+		<!-- Left: logo + title -->
+		<section class="flex items-center gap-1 cursor-pointer">
+			<img src="gemini/logo.png" alt="Logo" class="w-6.5 xs:w-5.5" />
+			<span class="hidden xs:block font-title font-semibold text-lg">GEMINI</span>
+		</section>
 
-    <!-- DESLTOP: Middle: links -->
-    <section class="hidden lg:flex justify-center items-center gap-5.5 pl-24">
-        <a href="/gemini">Prices</a>
-        <!-- All Products -->
-        <button class="group">
-            <!-- The button itself -->
-            <div class="flex items-center">
-                <span>All Products</span>
-                <img src="/gemini/chevron-down.png" alt="Dropdown arrow" />
-            </div>
-            <!-- Dropdown content -->
-            <div></div>
-        </button>
-        <!-- Institutions -->
-        <button class="group">
-            <!-- The button itself -->
-            <div class="flex items-center">
-                <span>Institutions</span>
-                <img src="/gemini/chevron-down.png" alt="Dropdown arrow" />
-            </div>
-            <!-- Dropdown content -->
-            <div></div>
-        </button>
-        <!-- Resources -->
-        <button class="group">
-            <!-- The button itself -->
-            <div class="flex items-center">
-                <span>Resources</span>
-                <img src="/gemini/chevron-down.png" alt="Dropdown arrow" />
-            </div>
-            <!-- Dropdown content -->
-            <div></div>
-        </button>
-    </section>
+		<!-- DESLTOP: Middle: links -->
+		<section class="hidden lg:flex justify-center items-center gap-5.5 pl-24">
+			<a href="/gemini">Prices</a>
+			<!-- All Products -->
+			<button class="group">
+				<!-- The button itself -->
+				<div class="flex items-center">
+					<span>All Products</span>
+					<img src="/gemini/chevron-down.png" alt="Dropdown arrow" />
+				</div>
+				<!-- Dropdown content -->
+				<div></div>
+			</button>
+			<!-- Institutions -->
+			<button class="group">
+				<!-- The button itself -->
+				<div class="flex items-center">
+					<span>Institutions</span>
+					<img src="/gemini/chevron-down.png" alt="Dropdown arrow" />
+				</div>
+				<!-- Dropdown content -->
+				<div></div>
+			</button>
+			<!-- Resources -->
+			<button class="group">
+				<!-- The button itself -->
+				<div class="flex items-center">
+					<span>Resources</span>
+					<img src="/gemini/chevron-down.png" alt="Dropdown arrow" />
+				</div>
+				<!-- Dropdown content -->
+				<div></div>
+			</button>
+		</section>
 
-    <!-- Right: ('Sign in' + 'Get started' links) + Mobile menu button -->
-    <section class="flex items-center gap-5 md:gap-6.5 lg:gap-5">
-        <!-- DESKTOP: 'Sign in' link -->
-        <a href="/gemini" class="hidden lg:block hover:opacity-100!">Sign in</a>
+		<!-- Right: ('Sign in' + 'Get started' links) + Mobile menu button -->
+		<section class="flex items-center gap-5.5 xs:gap-6.5 lg:gap-5">
+			<!-- DESKTOP: 'Sign in' link -->
+			<a href="/gemini" class="hidden lg:block hover:opacity-100!">Sign in</a>
 
-        <!-- Get start button -->
-        <a href="/gemini" class="px-4 py-2.5 font-semibold text-black bg-amber-400 rounded-full">Get started</a>
-        
-        <!-- MOBILE: hamburger menu -->
-        <button class="lg:hidden">
-            <img src="/gemini/menu.png" alt="Toggle mobile menu" class="w-7.5 h-5" />
-        </button>
-    </section>
-    </div>
+			<!-- Get start button -->
+			<a href="/gemini" class="orange-btn">Get started</a>
+
+			<!-- MOBILE: hamburger menu -->
+			<button class="lg:hidden">
+				<img src="/gemini/menu.png" alt="Toggle mobile menu" class="w-7.5 h-5" />
+			</button>
+		</section>
+	</div>
 </nav>
 
-<style lang="scss">
+<style>
+	@reference '../../../routes/(recreates)/gemini/gemini.css';
 
-@import "../../../routes/(recreates)/gemini/gemini.scss";
+	button,
+	a {
+		@apply transition;
 
-button, a {
-    &:hover {
-        @apply opacity-80 cursor-pointer;
-    }
-
-    img {
-        @apply w-5 h-4.5 transform transition-all group-hover:rotate-180;
-    }
-}
-
+		&:hover {
+			@apply opacity-80 cursor-pointer;
+		}
+		& img {
+			@apply w-5 mb-0.5 transform transition-all group-hover:rotate-180;
+		}
+		&.orange-btn {
+			@apply px-4 py-[9px] font-secondary text-[15px] text-secondary bg-orange-btn rounded-full;
+		}
+	}
 </style>
--- src/routes/(recreates)/gemini/+layout.svelte
+++ src/routes/(recreates)/gemini/+layout.svelte
@@ -1,11 +1,18 @@
 <script lang="ts">
     // Import components here!
     // NavBar and Footer
-    import './gemini.scss';
+    import './gemini.css';
     import NavBar from "$lib/components/gemini/NavBar.svelte";
 
     let { children } = $props();
 </script>
+
+<!-- Font import -->
+<svelte:head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Geist:wght@100..900&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+</svelte:head>
 
 <div class="w-full min-h-screen font-sans bg-white">
     <!-- Navbar here! -->
@@ -19,6 +26,6 @@
     <!-- Footer here! -->
 </div>
 
-<style lang="scss">
+<style>
 
 </style>
