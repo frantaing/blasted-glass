c6c60c59d49c3150ea747b341f633051ebba4192
frantan <francinetaing13@gmail.com>
2026-01-30 00:43:26 +0700

    refactor: compile repeating styles into classes in MobileMenu

--- src/lib/components/gemini/MobileMenu.svelte
+++ src/lib/components/gemini/MobileMenu.svelte
@@ -7,7 +7,7 @@
 </script>
 
 <div
-	class="lg:hidden flex flex-col gap-1 absolute top-full left-0 w-full h-screen overflow-none xs:px-[45px] pl-2.5 xs:pl-10 pr-3.5 pt-5 xs:pt-8 text-xl bg-secondary"
+	class="lg:hidden flex flex-col gap-1 absolute top-full left-0 w-full h-screen overflow-none xs:px-[45px] pl-4 xs:pl-12 pr-3.5 pt-5 xs:pt-6 text-xl bg-secondary"
 	in:fade={{ duration: 100 }}
 >
 	<!-- MAIN MENU VIEW -->
@@ -17,35 +17,26 @@
 			<a href="/gemini">Prices</a>
 
 			<!-- All Products Trigger -->
-			<button 
-				class="flex flex-col group w-full text-left"
-				onclick={() => activeSubmenu = 'products'}
-			>
-				<div class="navbar-dropdown justify-between w-full">
+			<button onclick={() => activeSubmenu = 'products'}>
+				<div class="trigger-text">
 					<span>All Products</span>
-					<img src="/gemini/chevron-down.png" alt="Arrow" class="w-6! opacity-60 -rotate-90" />
+					<img src="/gemini/chevron-down.png" alt="Arrow" />
 				</div>
 			</button>
 
 			<!-- Institutions Trigger -->
-			<button 
-				class="flex flex-col group w-full text-left"
-				onclick={() => activeSubmenu = 'institutions'}
-			>
-				<div class="navbar-dropdown justify-between w-full">
+			<button onclick={() => activeSubmenu = 'institutions'}>
+				<div class="trigger-text">
 					<span>Institutions</span>
-					<img src="/gemini/chevron-down.png" alt="Arrow" class="w-6! opacity-60 -rotate-90" />
+					<img src="/gemini/chevron-down.png" alt="Arrow" />
 				</div>
 			</button>
 
 			<!-- Resources Trigger -->
-			<button 
-				class="flex flex-col group w-full text-left"
-				onclick={() => activeSubmenu = 'resources'}
-			>
-				<div class="navbar-dropdown justify-between w-full">
+			<button onclick={() => activeSubmenu = 'resources'}>
+				<div class="trigger-text">
 					<span>Resources</span>
-					<img src="/gemini/chevron-down.png" alt="Arrow" class="w-6! opacity-60 -rotate-90" />
+					<img src="/gemini/chevron-down.png" alt="Arrow" />
 				</div>
 			</button>
 
@@ -56,25 +47,25 @@
 	<!-- SUBMENU: ALL PRODUCTS -->
 	{:else if activeSubmenu === 'products'}
 		<div class="flex flex-col gap-1 w-full text-xl">
-			<a href="#" class="group flex flex-col gap-2 w-full pl-3 py-3 hover:bg-primary/10 rounded-xl transition-colors">
+			<a href="#" class="group submenu-link">
 				<span class="text-sm">ActiveTrader</span>
-				<p class="font-normal text-[13px] opacity-70 group-hover:opacity-100 transition-opacity">Take full control over your trading experience.</p>
+				<p>Take full control over your trading experience.</p>
 			</a>
-			<a href="#" class="group flex flex-col gap-2 w-full pl-3 py-3 hover:bg-primary/10 rounded-xl transition-colors">
+			<a href="#" class="group submenu-link">
 				<span class="text-sm">Exchange</span>
-				<p class="font-normal text-[13px] opacity-70 group-hover:opacity-100 transition-opacity">Effortless buying and selling of over 90+ assets.</p>
+				<p>Effortless buying and selling of over 90+ assets.</p>
 			</a>
-			<a href="#" class="group flex flex-col gap-2 w-full pl-3 py-3 hover:bg-primary/10 rounded-xl transition-colors">
+			<a href="#" class="group submenu-link">
 				<span class="text-sm">Gemini Dollar</span>
-				<p class="font-normal text-[13px] opacity-70 group-hover:opacity-100 transition-opacity">Our USD-backed stablecoin.</p>
+				<p>Our USD-backed stablecoin.</p>
 			</a>
-			<a href="#" class="group flex flex-col gap-2 w-full pl-3 py-3 hover:bg-primary/10 rounded-xl transition-colors">
+			<a href="#" class="group submenu-link">
 				<span class="text-sm">Tokenized Stocks</span>
-				<p class="font-normal text-[13px] opacity-70 group-hover:opacity-100 transition-opacity">Trade U.S. stocks on the blockchain.</p>
+				<p>Trade U.S. stocks on the blockchain.</p>
 			</a>
-			<a href="#" class="group flex flex-col gap-2 w-full pl-3 py-3 hover:bg-primary/10 rounded-xl transition-colors">
+			<a href="#" class="group submenu-link">
 				<span class="text-sm">Staking</span>
-				<p class="font-normal text-[13px] opacity-70 group-hover:opacity-100 transition-opacity">Receive up to 12% APR on your crypto.</p>
+				<p>Receive up to 12% APR on your crypto.</p>
 			</a>
 		</div>
 
@@ -98,7 +89,19 @@
 
 	@reference '../../../routes/(recreates)/gemini/gemini.css';
 
-	.navbar-dropdown {
-		@apply flex items-center gap-0.5;
+	.trigger-text {
+		@apply flex justify-between items-center gap-0.5 w-full;
+
+		img {
+			@apply w-6! opacity-60 -rotate-90;
+		}
+	}
+
+	.submenu-link {
+		@apply flex flex-col gap-2 w-full pl-3 py-3 hover:bg-primary/10 rounded-xl transition-colors;
+
+		p {
+			@apply font-normal text-[13px] opacity-70 group-hover:opacity-100 transition-opacity;
+		}
 	}
 </style>
